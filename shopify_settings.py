079611206ccde8709567300e7653f9021f991b47
MK Rahal <mkrahal@users.noreply.github.com>
2019-06-10 03:10:16 +0100

    fixed bugs

--- shopify_settings.py
+++ shopify_settings.py
@@ -12,11 +12,11 @@
 import os
 
 # APP URL
-APP_DOMAINURL = 'https://cdn1.gdprinsider.ovh'  # !!!! IMPORTANT NO TRAILING '/' at the end of your domain
+APP_DOMAINURL = 'https://www.exampleGDPRapp.com'  # !!!! IMPORTANT NO TRAILING '/' at the end of your domain
 
 # API ACCESS
-SHOPIFY_API_KEY = '4fdc027a0c4d0e48044ae5ad4ef2ce19'
-SHOPIFY_API_SECRET = '80728c3d7509dd1d34c374852f6a0f20'
+SHOPIFY_API_KEY = 'XXXXXXXXXXXXXXX'
+SHOPIFY_API_SECRET = 'XXXXXXXXXXXXXX'
 
 # See http://api.shopify.com/authentication.html for available scopes
 # Set the permisssions your app will need.
@@ -27,11 +27,11 @@ SHOPIFY_API_SCOPE = ['read_customers', 'write_customers', 'write_script_tags', '
 APP_NAME = 'GDPR Compliance 360'
 APP_PRICE = 19.99  # Price in USD (always 2 decimal places)
 APP_TESTFLAG = True  # test flag to true will set the RecurringApplicationCharge to not actually charge the credit card
-APP_RETURNURL = "https://cdn1.gdprinsider.ovh/activatecharge/"  # The URL the customer is sent to when accept/decline charge.
+APP_RETURNURL = "https://www.exampleGDPRapp.com/activatecharge/"  # The URL the customer is sent to when accept/decline charge.
 APP_TRIALDAYS = 2
 
 # Email Service
 ACTIVE_TRANSACTIONALEMAIL_SERVICE = 'SENDGRID'  #SWITCH THIS TO 'SENDGRID' OR 'AMAZON' depending on what you want to use
-SENDGRID_API_KEY = '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'
-aws_id = 'AKIAJAW7QYGBW4XTQCFA'
-aws_secret ='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'
+SENDGRID_API_KEY = 'XXXXXXXXXXXXXX'
+aws_id = 'XXXXXXXXXXXXXX'
+aws_secret ='XXXXXXXXXXXXXX'
